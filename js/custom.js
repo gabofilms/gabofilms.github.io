b5099b98e3859648902879d7ecd301fe299e27b2
Franco Leonardi <francoreload@gmail.com>
2018-09-13 04:07:02 -0300

    Deleted useless files, now plays video on mobile too

--- js/custom.js
+++ js/custom.js
@@ -16,13 +16,8 @@
     }
 
     });
-
-*/
-    /*  IF YOU WANT TO USE DEVICE.JS TO DETECT THE VIEWPORT AND MANIPULATE THE OUTPUT  */
-
-        //Device.js will check if it is Tablet or Mobile - http://matthewhudson.me/projects/device.js/
-        if (!device.tablet() && !device.mobile()) {
-            myPlayer = $(".player").mb_YTPlayer({
+    */
+    myPlayer = $(".player").mb_YTPlayer({
             onReady: function( player ) {
                            var filters = {
                               grayscale: 0,
@@ -31,12 +26,5 @@
                            $(".pattern").removeClass("pattern");
             }
         });
-        } else {
-            //jQuery will add the default background to the preferred class 
-            $('.big-background, .small-background-section').addClass(
-                'big-background-default-image');
-        }
     });
-
-
 })(jQuery);
