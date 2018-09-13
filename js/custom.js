300c6d8dd3b718a9f7e4a5d4081100b22b8dcfd8
Franco Leonardi <francoreload@gmail.com>
2018-09-13 20:14:08 -0300

    Fixed logo, removed subtext

--- js/custom.js
+++ js/custom.js
@@ -1,22 +1,6 @@
 (function($) {
     $(document).ready(function() {
 
-    /* IF YOU WANT TO APPLY SOME BASIC JQUERY TO REMOVE THE VIDEO BACKGROUND ON A SPECIFIC VIEWPORT MANUALLY
-
-     var is_mobile = false;
-
-    if( $('.player').css('display')=='none') {
-        is_mobile = true;       
-    }
-    if (is_mobile == true) {
-        //Conditional script here
-        $('.big-background, .small-background-section').addClass('big-background-default-image');
-    }else{
-        $(".player").mb_YTPlayer(); 
-    }
-
-    });
-    */
     myPlayer = $(".player").mb_YTPlayer({
             onReady: function( player ) {
                            var filters = {
